b67e6f384954497286b97057323854b16eef8bc3
Shilpa519 <srinivasshilpa05@gmail.com>
2023-04-14 09:46:03 +0000

    first commit

---
+++ src/components/Home/styledComponents.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Title = styled.h1`
+  color: #334155;
+  font-weight: 700;
+  font-size: 45px;
+`
+
+export const Para = styled.p`
+  color: #475569;
+  font-size: 30px;
+`
+
+export const RegisterButton = styled.button`
+  color: #ffffff;
+  font-size: 15px;
+  padding: 15px;
+  background-color: #3b82f6;
+  border-color: transparent;
+  cursor: pointer;
+`
+
+export const MeetImage = styled.img`
+  height: 650px;
+  width: 850px;
+`
